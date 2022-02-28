05bfa5b48599e775b2d03b61f0b78b9b54a47416
Ruslan <danilov2001@mail.ru>
2022-02-28 20:39:52 +0300

    handling some exceptions

--- src/main/java/com/company/controllers/ExceptionController.java
+++ src/main/java/com/company/controllers/ExceptionController.java
@@ -5,8 +5,11 @@ import com.company.exception.ResourceNotFound;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 
@@ -15,9 +18,20 @@ public class ExceptionController extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({ResourceNotFound.class})
     public ResponseEntity<Object> handleNotFound(ResourceNotFound ex) {
-        String errors = ex.toString();
         System.out.println("ExceptionHandler.handleNotFound");
-        ApiError error = new ApiError(HttpStatus.NOT_FOUND, ex.getLocalizedMessage(), errors);
+        ApiError error = new ApiError(HttpStatus.NOT_FOUND, ex.getLocalizedMessage());
         return new ResponseEntity<>(error, new HttpHeaders(), error.getStatus());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ApiError err = new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage());
+        return new ResponseEntity<>(err, new HttpHeaders(), err.getStatus());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ApiError err = new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage());
+        return new ResponseEntity<>(err, new HttpHeaders(), err.getStatus());
     }
 }
