e13dc75d574b9708bd801f2e5e332bf365a1431b
Ruslan <danilov2001@mail.ru>
2022-02-28 17:05:23 +0300

    handling not found exception

--- src/main/java/com/company/controllers/ExceptionController.java
+++ src/main/java/com/company/controllers/ExceptionController.java
@@ -1,6 +1,7 @@
 package com.company.controllers;
 
 import com.company.exception.ApiError;
+import com.company.exception.ResourceNotFound;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,22 +9,14 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.ConstraintViolationException;
-import java.util.ArrayList;
-import java.util.List;
 
 @ControllerAdvice
 public class ExceptionController extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler({ConstraintViolationException.class})
-    public ResponseEntity<Object> handleNotFound(ConstraintViolationException ex) {
-        List<String> errors = new ArrayList<>();
+    @ExceptionHandler({ResourceNotFound.class})
+    public ResponseEntity<Object> handleNotFound(ResourceNotFound ex) {
+        String errors = ex.toString();
         System.out.println("ExceptionHandler.handleNotFound");
-        for (ConstraintViolation<?> violation : ex.getConstraintViolations()) {
-            errors.add(violation.getRootBeanClass().getName() + " " +
-                    violation.getPropertyPath() + ": " + violation.getMessage());
-        }
         ApiError error = new ApiError(HttpStatus.NOT_FOUND, ex.getLocalizedMessage(), errors);
         return new ResponseEntity<>(error, new HttpHeaders(), error.getStatus());
     }
