fcd0e29b2080ad0b6576af56e7f2a2cbc406e335
Ruslan <danilov2001@mail.ru>
2022-03-01 14:01:24 +0300

    adding timestamp to response exception

--- src/main/java/com/company/controllers/ExceptionController.java
+++ src/main/java/com/company/controllers/ExceptionController.java
@@ -12,6 +12,8 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.time.LocalDateTime;
+
 
 @ControllerAdvice
 public class ExceptionController extends ResponseEntityExceptionHandler {
@@ -19,19 +21,19 @@ public class ExceptionController extends ResponseEntityExceptionHandler {
     @ExceptionHandler({ResourceNotFound.class})
     public ResponseEntity<Object> handleNotFound(ResourceNotFound ex) {
         System.out.println("ExceptionHandler.handleNotFound");
-        ApiError error = new ApiError(HttpStatus.NOT_FOUND, ex.getLocalizedMessage());
+        ApiError error = new ApiError(HttpStatus.NOT_FOUND, ex.getLocalizedMessage(), LocalDateTime.now());
         return new ResponseEntity<>(error, new HttpHeaders(), error.getStatus());
     }
 
     @Override
     protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        ApiError err = new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage());
+        ApiError err = new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), LocalDateTime.now());
         return new ResponseEntity<>(err, new HttpHeaders(), err.getStatus());
     }
 
     @Override
     protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        ApiError err = new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage());
+        ApiError err = new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), LocalDateTime.now());
         return new ResponseEntity<>(err, new HttpHeaders(), err.getStatus());
     }
 }
--- src/main/java/com/company/exception/ApiError.java
+++ src/main/java/com/company/exception/ApiError.java
@@ -1,18 +1,24 @@
 package com.company.exception;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
 
 
 @Data
 public class ApiError {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    private LocalDateTime timestamp;
     private HttpStatus status;
     private String message;
 
-    public ApiError(HttpStatus status, String message) {
+    public ApiError(HttpStatus status, String message, LocalDateTime timestamp) {
         super();
         this.status = status;
         this.message = message;
+        this.timestamp = timestamp;
     }
 
 }
